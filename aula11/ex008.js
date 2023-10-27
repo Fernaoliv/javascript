8f5bac489f1961c8992e106cfdc651da0cfe562b
Fernaoliv <fernandoblue3@gmail.com>
2023-10-27 19:11:11 -0300

    quarto dia
    devagar e sempre

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = prompt('Insira a velocidade do seu veiculo: ')
+
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel >= 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!!!')
+}
+
+    console.log('Dirija sempre usando sinto de segurança')
+
