df4072219bc1d7418ace9c3598e90ad534072f33
Fernaoliv <fernandoblue3@gmail.com>
2023-11-03 16:21:00 -0300

    hoje

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,49 @@
-alert(`Olá`)
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        alert('[ERRO!] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/foto-crianca-m.png')
+            } else if (idade < 22) {
+                // Jovem
+                img.setAttribute('src', 'imagens/foto-jovem-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/foto-adulto-m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/foto-idoso-m.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/foto-crianca-f.png')
+            } else if (idade < 22) {
+                // Jovem
+                img.setAttribute('src', 'imagens/foto-jovem-f.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/foto-adulto-f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/foto-idoso-f.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
